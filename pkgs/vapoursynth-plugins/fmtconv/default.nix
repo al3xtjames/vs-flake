ac4cf808051024a4505a329245a1577dc7ae31e4
Alex James <git@alextjam.es>
2023-09-16 17:08:01 -0500

    vapoursynthPlugins.fmtconv: unstable-2023-01-28 -> unstable-2023-09-02

--- pkgs/vapoursynth-plugins/fmtconv/default.nix
+++ pkgs/vapoursynth-plugins/fmtconv/default.nix
@@ -1,18 +1,18 @@
 { lib
 , stdenv
-, fetchFromGitHub
+, fetchFromGitLab
 , autoreconfHook
 }:
 
 stdenv.mkDerivation rec {
   pname = "vapoursynth-fmtconv";
-  version = "unstable-2023-01-28";
+  version = "unstable-2023-09-02";
 
-  src = fetchFromGitHub {
+  src = fetchFromGitLab {
     owner = "EleonoreMizo";
     repo = "fmtconv";
-    rev = "3eec42f8aaf86f3327b9190c6b25a0c9eca22028";
-    hash = "sha256-odgnkH3Ofhbz+PSc0BHm3rlJMNsHg089wIetOxCZi8Q=";
+    rev = "f2d8f825aad873284e56baac081393c0f7320b35";
+    hash = "sha256-3UnboHDeE1bnTV1djyoG6ONUbBEsbYNiPfIjDe5+AfI=";
   };
 
   nativeBuildInputs = [
@@ -31,7 +31,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Format conversion tools for Vapoursynth and Avisynth+";
-    homepage = "https://github.com/EleonoreMizo/fmtconv";
+    homepage = "https://gitlab.com/EleonoreMizo/fmtconv";
     license = licenses.wtfpl;
     maintainers = with maintainers; [ ];
     platforms = platforms.all;
