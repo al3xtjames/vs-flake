a39b52267ae4913bc8c9f448714c58edd37c7d8c
Alex James <git@alextjam.es>
2023-11-19 00:06:13 -0600

    vapoursynthPlugins.subtext: 4 -> 5

--- pkgs/vapoursynth-plugins/subtext/default.nix
+++ pkgs/vapoursynth-plugins/subtext/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "subtext";
-  version = "4";
+  version = "5";
 
   src = fetchFromGitHub {
     owner = "vapoursynth";
     repo = pname;
     rev = "R${version}";
-    hash = "sha256-Tux8WFbUn4Bt1EL9r+f+Y/av9w9Y23gc79m1JcZWj50=";
+    hash = "sha256-U4KsyR2sf0O4mG6o3JK7RTsWd/BDMJfXHP7xTCRHXXE=";
   };
 
   postPatch = ''
